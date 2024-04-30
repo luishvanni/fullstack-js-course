28bbc9b7886353886f5896ebbeec470df69a3878
Luís Henrique Vanni <luishvanni@gmail.com>
2024-04-30 19:07:06 -0300

    matrizes em js

--- javascript-modulo-02/aula-36-matriz/script.js
+++ javascript-modulo-02/aula-36-matriz/script.js
@@ -1,8 +1,34 @@
 const arr = [
-    "Nível 1",
-    "Nível 2",
-    "Nível 3"  
-]
+    "1º Nível",
+    ["2º nível", 42, true],
+    [
+      ["3º nível, 1º item", "Olá, mundo!"],
+      ["3º nível, 2º item", "Oi, mundo!"],
+    ],
+    []
+  ]
 
 console.log(arr);
-alert
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1][0]) //Acessar valor de um array dentro de um array 
+console.log(arr[1][1]) //Acessar valor de um array dentro de um array 
+console.log(arr[2])
+console.log(arr[2][1])
+console.log(arr[2][1][1])
+
+
+const matriz = [
+    ["l1, c1", "l1, c2", "l1, c3", "l1, c4"],
+    ["l2, c1", "l2, c2", "l2, c3", "l2, c4"],
+    ["l3, c1", "l3, c2", "l3, c3", "l3, c4"],
+  ]
+  console.log(matriz)
+
+  //usar vários níveis de laços FOR para iterar facilmente pela matriz
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+      const elemento = matriz[i][j]
+      console.log("Posição: (" + i + ", " + j + ") Valor: " + elemento)
+    }
+  }
