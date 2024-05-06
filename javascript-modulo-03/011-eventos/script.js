a91e77b0b6e693f3f51425aa92954d6d658f4a84
Luís Henrique Vanni <luishvanni@gmail.com>
2024-05-05 21:39:48 -0300

    eventos no js

---
+++ javascript-modulo-03/011-eventos/script.js
@@ -0,0 +1,33 @@
+function register(ev) {
+    console.log(ev)
+    const sectionElement = ev.currentTarget.parentNode
+    const username = sectionElement.children.username.value
+    const password = sectionElement.children.password.value
+    const passwordConfirmation = sectionElement.children.passwordConfirmation.value
+  
+    if (password === passwordConfirmation) {
+      alert("Usuário " + username + " registrado!")
+    } else {
+      alert("As senhas não conferem")
+    }
+  }
+  
+  function removeEvent() {
+    button.removeEventListener("click", register)
+    alert("Event Removed")
+  }
+  
+  const button = document.getElementById("register-button")
+  
+  button.addEventListener("click", register)
+  
+  button.addEventListener("mouseover", function (ev) {
+    console.log(ev)
+  })
+
+
+
+//---- ADD E REMOVE EventListener ----
+//1-string que será "ouvida"
+//2-função que será chamada
+//adicionar uma funcionalidade no botom ->evento
