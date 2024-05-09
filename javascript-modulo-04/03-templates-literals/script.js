a759e5678bf7cb70396be1e5f575ebe23b10293e
Luís Henrique Vanni <luishvanni@gmail.com>
2024-05-09 18:48:22 -0300

    arrows functions

---
+++ javascript-modulo-04/03-templates-literals/script.js
@@ -0,0 +1,5 @@
+const name = 'Luis'
+
+console.log(`Olá, ${name}!
+Estamos em ${Date()}
+    2 + 2 = ${2 + 2}`);
---
+++ javascript-modulo-04/05-arrows-functions/script.js
@@ -0,0 +1,30 @@
+function normalSum(a, b) {
+    return a + b
+}
+
+console.log(`Soma normal: ${normalSum(2, 2)}`)
+
+
+const anonymousSum = function (a, b) {
+    return a + b
+}
+
+console.log(`Soma anônima: ${anonymousSum(2, 2)}`)
+
+//arrow function
+const arrowSum = (a, b) => {
+    return a + b
+}
+console.log(`Soma Arrow Function: ${arrowSum(2, 2)}`)
+
+const subtract = (a, b) => a - b
+console.log(`Subtração: ${subtract(6, 2)}`)
+
+const double = n => `O dobro de ${n} é ${n*2}`
+const number = 21
+console.log(double(number))
+
+const towns = ['Prontera', 'Izlude', 'Payon', 'Alberta', 'Geffen', 'Morroc']
+
+const startingWithP = towns.filter(town => town[0] === 'P')
+console.log(startingWithP)
