d5b5d081ff56e833281298fb975b878a7a12f31b
Luís Henrique Vanni <luishvanni@gmail.com>
2024-05-10 14:34:17 -0300

    Rest Params

---
+++ javascript-modulo-04/09-rest-params/script.js
@@ -0,0 +1,10 @@
+function sum(...numbers) {
+    return numbers.reduce((accum, num) => accum + num, 0)
+}
+
+console.log(sum(1, 1))
+console.log(sum(2, 2, 2, 2, 2))
+console.log(sum(32, 5, 74, 7, 1, 9, 90))
+
+//passar quantos parametros eu quiser
+//pode manipular parametros como arrays
