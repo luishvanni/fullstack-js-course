841d43e32a9f1fb96cea56d5c06933c45b1f9384
Luís Henrique Vanni <122712010+luishvanni@users.noreply.github.com>
2024-06-17 18:28:33 -0300

    exercício 20 - Classes para o DOM

--- javascript-modulo-05/exercicios/ex020/Component.js
+++ javascript-modulo-05/exercicios/ex020/Component.js
@@ -1,4 +1,25 @@
 export class Component {
     #element = null
-    
+
+    constructor(tag, parent, options) {
+        this.tag = tag
+        this.parent = parent
+        this.options = options
+        this.build()
+    }
+
+    build() {
+        this.#element = document.createElement(this.tag)
+        Object.assign(this.#element, this.options)
+        return this //retorna a prórpia instância
+    }
+
+    render() {
+        if (this.parent instanceof Component) {
+            this.parent.getElement().append(this.#element)
+        } else {
+            document.querySelector(this.parent).append(this.#element)
+        }
+    }
 }
+
